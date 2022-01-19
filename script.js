bd2f97bcb7a4e44fd182f05a2fa6d27e9fd2b948
Victoria <Lvictoria159753@gmail.com>
2022-01-18 18:17:09 -0800

    got user input printing

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 let myLibrary=[];
-
+/*
 let i;
-myBook ()=>{
+function myBook (){
 for (i=0; myLibrary.length; i++){
     myLibrary[i];
-    console.log(myLibrary[i])
+    console.log(myLibrary[i]);
 }
-
-}
-
+};
+*/
+/*
 addBookToLibrary ()=>{
 
 }
+*/
+
+
+
+
+function print() {
+    document.getElementById("display").innerHTML = document.getElementById('title').value + " " + 
+document.getElementById('author').value + " " +
+document.getElementById('pages').value;
+  }
