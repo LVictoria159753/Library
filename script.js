daf06c3d5f337dfb094d2cf15810f898b7879110
Victoria <Lvictoria159753@gmail.com>
2022-01-23 20:08:23 -0800

    added Library objects and constructors

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
 let myLibrary=[];
-/*
-let i;
-function myBook (){
+console.log(myLibrary);
+
+//Object Constructor
+function myBook (author, title, pages, read){
+    this.author= author;
+    this.title=title;
+    this.pages=pages;
+    this.read=read;
+    this.summary = function (){
+      return ('${this.title}','${this.author}', '${this.pages}', '${this.read}');
+    }
+};
+
+function addBookToLibrary (book){
+  myLibrary.push(book);
+}
+
+
+
+function displayLibrary (){
 for (i=0; myLibrary.length; i++){
     myLibrary[i];
     console.log(myLibrary[i]);
-}
 };
-*/
-/*
-addBookToLibrary ()=>{
-
-}
-*/
 
 
+let submit=document.querySelector("#submit")
+
+submit.addEventListener("click", print);
 
 
 function print() {
-    document.getElementById("display").innerHTML = document.getElementById('title').value + " " + 
+ document.getElementById("display").innerHTML = document.getElementById('title').value + " " + 
 document.getElementById('author').value + " " +
 document.getElementById('pages').value;
   }
+
+
