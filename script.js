1c104940dd410f660cc0d4d1cece983ca42f7cdb
Victoria <Lvictoria159753@gmail.com>
2022-01-25 21:44:00 -0800

    print the array on HTML site

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 
 let myLibrary=[
-    {title: "A Game of Thrones",
-    author: "George R. R. Martin",
-    pages: 694,
-    read: false}
 ];
 console.log(myLibrary);
 
@@ -24,28 +20,34 @@ function addBookToLibrary (){
   title = inputTitle.value;
   author = inputPages.value;
   pages = inputAuthor.value;
-  summary = new myBook ( this.title, this.author, this.pages)
+  read= inputRead.value;
+  summary = new myBook ( this.title, this.author, this.pages, this.read)
   myLibrary.push(summary);
 }
 
 
-/*
+
+
 function displayLibrary (){
 for (let i=0;i<myLibrary.length; i++){
     console.log(myLibrary[i]);
-};
-*/
+    arrayContents=JSON.stringify(myLibrary)
+    let displayBooks=document.querySelector("#displayBooks").innerHTML=arrayContents;
+}
+}
+
 
 //Allows the table values to be populated.
 let submit=document.querySelector("#submit");
 submit.addEventListener('click', ()=>{
- print();
- addBookToLibrary();
+  print();
+  addBookToLibrary();
+  displayLibrary();
 });
 
 function print() {
     document.getElementById("displayTitle").innerHTML = document.getElementById('title').value + " ";
     document.getElementById("displayAuthor").innerHTML=document.getElementById('author').value + " " ;
     document.getElementById("displayPages").innerHTML=document.getElementById('pages').value +" ";
-
+    document.getElementById("displayRead").innerHTML=document.getElementById('read').value +" ";
   }
